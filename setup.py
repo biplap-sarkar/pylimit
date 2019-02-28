2e3e1ddfc8183202ad6ed75f163e66338f5e9d63
Pablo Hernandez <pablo.hernandez@liteview.imaginefulfillment.com>
2019-02-28 11:15:56 -0800

    bumped-version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pylimit',
   packages = ['pylimit'],
-  version = '0.1.7',
+  version = '0.1.8',
   description = 'A distributed rate limiting library for python using leaky bucket algorithm and Redis',
   author='Biplap Sarkar',
   author_email='biplap.sarkar@gmail.com',
