547a59833aa44b6eda2959c1e1cbd286aca76ab7
Biplap Sarkar <biplap.sarkar@gmail.com>
2023-08-05 21:23:54 -0700

    updated release version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pylimit',
   packages = ['pylimit'],
-  version = '0.1.7',
+  version = '1.0.0',
   description = 'A distributed rate limiting library for python using leaky bucket algorithm and Redis',
   author='Biplap Sarkar',
   author_email='biplap.sarkar@gmail.com',
   url='https://github.com/biplap-sarkar/pylimit',
-  download_url='https://github.com/biplap-sarkar/pylimit/archive/v0.1.7.tar.gz',
+  download_url='https://github.com/biplap-sarkar/pylimit/archive/v1.0.0.tar.gz',
   keywords=['rate limiting', 'throttle', 'redis'],
   classifiers=[],
 )
